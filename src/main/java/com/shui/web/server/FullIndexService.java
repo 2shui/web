2984713f9c60735b8c39bfc4ed03627ef2567692
zgj <zgjlovelife@gmail.com>
2016-05-19 18:29:36 +0800

    2016-5-19 18:29:34

--- src/main/java/com/shui/web/server/FullIndexService.java
+++ src/main/java/com/shui/web/server/FullIndexService.java
@@ -34,7 +34,7 @@ public class FullIndexService {
 			for (Document doc : documents) {
 				Page page = new Page();
 				page.setTitle(doc.get(AppConfig.INDEX_TITLE));
-				page.setUri("http://" + AppConfig.WEB_SITE + "/"
+				page.setFileName("http://" + AppConfig.WEB_SITE + "/"
 						+ doc.get(AppConfig.INDEX_FILE_NAME) + ".html");
 				page.setContent(doc.get(AppConfig.INDEX_CONTENT));
 				result.add(page);
