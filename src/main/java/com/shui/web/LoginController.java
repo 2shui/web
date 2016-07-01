dfab04c45a015591f01f3c22a62736b6073aeb3f
zgj <zgjlovelife@gmail.com>
2016-07-01 17:17:39 +0800

    2016-7-1 17:17:37

--- src/main/java/com/shui/web/LoginController.java
+++ src/main/java/com/shui/web/LoginController.java
@@ -10,10 +10,9 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.qq.connect.QQConnectException;
 import com.shui.web.repo.MemberMapper;
 import com.shui.web.server.AssociatorService;
-import com.shui.weibo.Oauth;
-import com.shui.weibo.http.AccessToken;
 import com.shui.weibo.model.WeiboException;
 
 @SpringBootApplication
@@ -30,16 +29,31 @@ public class LoginController {
 	public String access(HttpServletRequest request) throws WeiboException {
 		request.getSession().setAttribute("login_current_url",
 				request.getHeader("Referer"));
-		Oauth oauth = new Oauth();
+		com.shui.weibo.Oauth oauth = new com.shui.weibo.Oauth();
 		return "redirect:" + oauth.authorize("code");
 	}
 
 	@RequestMapping("/weibo_redirect")
 	public String redirect(@RequestParam String code, HttpServletRequest request, HttpServletResponse resp)
 			throws WeiboException {
-		Oauth oauth = new Oauth();
-		AccessToken token = oauth.getAccessTokenByCode(code);
+		com.shui.weibo.Oauth oauth = new com.shui.weibo.Oauth();
+		com.shui.weibo.http.AccessToken token = oauth.getAccessTokenByCode(code);
 		return "redirect:" + associatorService.weiboLogin(request, resp, token);
 	}
 	
+	@RequestMapping("/qq")
+	public String accessQQ(HttpServletRequest request) throws QQConnectException {
+		request.getSession().setAttribute("login_current_url",
+				request.getHeader("Referer"));
+		com.qq.connect.oauth.Oauth oauth = new com.qq.connect.oauth.Oauth();
+		return "redirect:" + oauth.getAuthorizeURL(request);
+	}
+
+	@RequestMapping("/qq_redirect")
+	public String redirectQQ(@RequestParam String code, HttpServletRequest request, HttpServletResponse resp)
+			throws QQConnectException {
+		com.qq.connect.oauth.Oauth oauth = new com.qq.connect.oauth.Oauth();
+		com.qq.connect.javabeans.AccessToken token = oauth.getAccessTokenByRequest(request);
+		return "redirect:" + associatorService.qqLogin(request, resp, token);
+	}
 }
--- src/main/java/com/shui/web/server/AssociatorService.java
+++ src/main/java/com/shui/web/server/AssociatorService.java
@@ -11,6 +11,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.mysql.jdbc.StringUtils;
+import com.qq.connect.QQConnectException;
+import com.qq.connect.api.OpenID;
+import com.qq.connect.api.qzone.UserInfo;
+import com.qq.connect.javabeans.qzone.UserInfoBean;
 import com.shui.web.conf.AppConfig;
 import com.shui.web.model.Member;
 import com.shui.web.repo.MemberMapper;
@@ -28,24 +33,26 @@ public class AssociatorService {
 	private MemberMapper memberMapper;
 
 	public String weiboLogin(HttpServletRequest request, HttpServletResponse resp, AccessToken token) {
-		Member m = memberMapper.getByOpenidAndType(token.getUid(), "weibo");
-		if(null == m) {
-			try {
-				User user = new Users(token.getAccessToken()).showUserById(token.getUid());
-				String uid = user.getId();
-				String userName = user.getName();
-				String profileImage = user.getProfileImageUrl();
-				String gender = user.getGender();
-				Member member = new Member(uid, userName, new Date(), "weibo", gender, null, profileImage);
-				memberMapper.reg(member);
-				m = memberMapper.getByOpenidAndType(uid, "weibo");
-			} catch (WeiboException e) {
-				log.error("weibo token showUserById error:{}", e);
-				e.printStackTrace();
+		if(!StringUtils.isNullOrEmpty(token.getAccessToken())) {
+			Member m = memberMapper.getByOpenidAndType(token.getUid(), "weibo");
+			if(null == m) {
+				try {
+					User user = new Users(token.getAccessToken()).showUserById(token.getUid());
+					String uid = user.getId();
+					String userName = user.getName();
+					String profileImage = user.getProfileImageUrl();
+					String gender = user.getGender();
+					Member member = new Member(uid, userName, new Date(), "weibo", gender, null, profileImage);
+					memberMapper.reg(member);
+					m = memberMapper.getByOpenidAndType(uid, "weibo");
+				} catch (WeiboException e) {
+					log.error("weibo token showUserById error:{}", e);
+					e.printStackTrace();
+				}
 			}
+			Cookies.set(resp, AppConfig.LOGIN_NAME, m.getNickName());
+			request.getSession().setAttribute(AppConfig.BOOK_LOGIN, m);
 		}
-		Cookies.set(resp, AppConfig.LOGIN_NAME, m.getNickName());
-		request.getSession().setAttribute(AppConfig.BOOK_LOGIN, m);
 		
 		String uri = (String) request.getSession().getAttribute("login_current_url");
 		request.getSession().removeAttribute("login_current_url");
@@ -53,8 +60,34 @@ public class AssociatorService {
 		return uri;
 	}
 	
-	public void tet(HttpServletRequest request, Member m) {
-		request.getSession().setAttribute(AppConfig.BOOK_LOGIN, m);
+	public String qqLogin(HttpServletRequest request, HttpServletResponse resp, com.qq.connect.javabeans.AccessToken token) {
+		String accessToken = token.getAccessToken();
+		if(!StringUtils.isNullOrEmpty(accessToken)) {
+			OpenID openIDObj =  new OpenID(accessToken);
+			try {
+				String openID = openIDObj.getUserOpenID();
+				Member m = memberMapper.getByOpenidAndType(openID, "qq");
+				if(null == m) {
+					UserInfo qzoneUserInfo = new UserInfo(accessToken, openID);
+					UserInfoBean userInfoBean = qzoneUserInfo.getUserInfo();
+					String userName = userInfoBean.getNickname();
+					String profileImage = userInfoBean.getAvatar().getAvatarURL30();
+					String gender = userInfoBean.getGender();
+					Member member = new Member(openID, userName, new Date(), "weibo", gender, null, profileImage);
+					memberMapper.reg(member);
+					m = memberMapper.getByOpenidAndType(openID, "qq");
+				}
+				Cookies.set(resp, AppConfig.LOGIN_NAME, m.getNickName());
+				request.getSession().setAttribute(AppConfig.BOOK_LOGIN, m);
+			} catch (QQConnectException e) {
+				log.error("qq access error:{}", e);
+				e.printStackTrace();
+			}
+		}
+		
+		String uri = (String) request.getSession().getAttribute("login_current_url");
 		request.getSession().removeAttribute("login_current_url");
+		uri = null == uri || uri.isEmpty() ? AppConfig.FULL_SITE : uri;
+		return uri;
 	}
 }
